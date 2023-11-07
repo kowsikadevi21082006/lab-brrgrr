f904701a44bed04d3d2a3dfeb9807ad4937d9d2a
kowsikadevi21082006 <144236845+kowsikadevi21082006@users.noreply.github.com>
2023-11-07 10:06:41 +0530

    done

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@
 // unless absolutely needed.
 
 //Initial price of the burger
-var wholeWheatBun = 10;
-
+var wholeWheatBun = 20;
+ let price = document.getElementById("price-details");
+  price.innerText='INR 170';
 //Ingredients of the burger along with the price
 // Clue: the name is same as the textcontent of the button. Will be useful later on :)
 var ingredients = {
@@ -30,7 +31,7 @@ function renderAll() {
   renderTomatoes();
   renderOnions();
   renderLettuce();
-  renderButtons();
+//   renderButtons();
   renderIngredientsBoard();
   renderPrice();
 }
@@ -46,44 +47,179 @@ function renderPatty() {
 }
 
 function renderCheese() {
-  //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+  let cheese = document.querySelector("#cheese");
+  //you can also use getElementById
+  if (state.Cheese) {
+    cheese.style.display = "inherit";
+  } else {
+    cheese.style.display = "none";
+  }
 }
 
 function renderTomatoes() {
-  //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+  let tomato = document.querySelector("#tomato");
+  //you can also use getElementById
+  if (state.Tomatoes) {
+    tomato.style.display = "inherit";
+  } else {
+    tomato.style.display = "none";
+  }
 }
 
 function renderOnions() {
-  //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+  let onion = document.querySelector("#onion");
+  //you can also use getElementById
+  if (state.Onions) {
+    onion.style.display = "inherit";
+  } else {
+    onion.style.display = "none";
+  }
 }
 
 function renderLettuce() {
-  //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+  let lettuce = document.querySelector("#lettuce");
+  //you can also use getElementById
+  if (state.Lettuce) {
+    lettuce.style.display = "inherit";
+  } else {
+    lettuce.style.display = "none";
+  }
 }
-
 document.querySelector(".btn-patty").onclick = function () {
   state.Patty = !state.Patty;
-  renderAll();
+  renderAll();renderPrice()
 };
 
 // Trial 2 - Setup event listener for the cheese button
-
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  renderAll();renderPrice()
+};
 
 // Trial 2 - Setup event listener for the tomatoes button
-
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  renderAll();renderPrice()
+};
 
 // Trial 2 - Setup event listener for the onion button
 
-
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  renderAll();renderPrice()
+};
 // Trial 2 - Setup event listener for the lettuce button
-
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  renderAll();renderPrice()
+};
 
 //Challenge 1 - Add/Remove the class active to the buttons based on state
-
+let btpatty = document.querySelector(".btn-patty")
+let btcheese = document.querySelector(".btn-cheese")
+let bttomato = document.querySelector(".btn-tomatoes")
+let btonion = document.querySelector(".btn-onions")
+let btlettuce = document.querySelector(".btn-lettuce")
+document.querySelector(".btn-patty").onclick = function () {
+  state.Patty = !state.Patty;
+  if(state.Patty){
+    btpatty.classList.add("active")
+  }else{
+    btpatty.classList.remove("active")
+  }
+  renderAll();renderPrice()
+};
+document.querySelector(".btn-cheese").onclick = function () {
+  state.Cheese = !state.Cheese;
+  if(state.Cheese){
+    btcheese.classList.add("active")
+  }else{
+    btcheese.classList.remove("active")
+  }
+  renderAll();renderPrice()
+};
+document.querySelector(".btn-tomatoes").onclick = function () {
+  state.Tomatoes = !state.Tomatoes;
+  if(state.Tomatoes){
+    bttomato.classList.add("active")
+  }else{
+    bttomato.classList.remove("active")
+  }
+  renderAll();renderPrice()
+};
+document.querySelector(".btn-onions").onclick = function () {
+  state.Onions = !state.Onions;
+  if(state.Onions){
+    btonion.classList.add("active")
+  }else{
+    btonion.classList.remove("active")
+  }
+  renderAll();renderPrice()
+};
+document.querySelector(".btn-lettuce").onclick = function () {
+  state.Lettuce = !state.Lettuce;
+  if(state.Lettuce){
+    btlettuce.classList.add("active")
+  }else{
+    btlettuce.classList.remove("active")
+  }
+  renderAll();
+  renderPrice()
+};
 
 //Challenge 2 - Render only the items selected in the ingredients board based on the state
+function renderIngredientsBoard(){
+  const items = document.querySelectorAll(".items")
+  for(i=0;i<items.length;i++){
+    let item = items[i].textContent
+    console.log(item)
+    if(state[item]!=true){
+      items[i].style.display="none"
+    }
+    else {
+      items[i].style.display="block"
+    }
+  
+  }}
+  
+  //Judgement 1
+  //In the p element having price-details as the class, display the calculated
+  //price based on ingredients
+  
+  function renderPrice(){
+    let total = calculate();
+    price.innerText = `INR ${total}`;
 
-
-//Judgement 1
-//In the p element having price-details as the class, display the calculated
-//price based on ingredients
+  
+    function calculate() {
+      let total = wholeWheatBun;
+      for (let i in ingredients){
+        if(state[i]){
+          total += ingredients[i]
+        }
+        console.log(total)
+      }return total; 
+    }
+  }
+  
+  const pay =  document.querySelector(".Rectangle5")
+  pay.addEventListener('click', function () {
+     let paid = confirm("Do you want to pay?")
+      if(paid){
+        let add = confirm("You have paid the bill😍😀")
+        if (add){
+          confirm("Here is some cashback for you ")
+        }else {
+          confirm("Have a good day!!..")
+        }
+      }else if(paid!==true) {
+        let no = confirm(" Go back and wash the Dishes😡")
+        if(no){
+          confirm("Good boy!Clean the dishes carefully")
+        }else{
+          confirm("Call the POLICE!!..")
+        }
+      }
+    }
+  );
+  
